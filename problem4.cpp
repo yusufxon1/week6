b6f8853e3619eed53b1e9c23cc789580048dc923
yusufxon1 <147371884+yusufxon1@users.noreply.github.com>
2024-04-09 22:41:59 +0500

    problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,52 @@
+//by saidjonakr
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void heapify(long long arr[], int n, int i){
+    int largest = i; 
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+        
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(long long arr[], int n){
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+    for (int i = n - 1; i >= 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main(){
+  int n;
+  cin >> n;
+  long long arr[n];
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  for(int i = n/2-1; i >= 0; i--){
+      heapify(arr, n, i);
+  }
+  heapSort(arr, n);
+  long long mn = INT_MAX; 
+  for(int i = 1; i < n; i++){
+    mn = min(mn, arr[i] - arr[i-1]);
+  }
+  for(int i = 1; i < n; i++){
+    if(arr[i] - arr[i-1] == mn){
+      cout << arr[i - 1] << " " << arr[i] << endl;
+    }
+  }
+  return 0;
+}
